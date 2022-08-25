a4acd11663c8f938d4eb4fee8de30bfdc31c8fe8
Alex Mihailov <ybahop@gmail.com>
2022-08-25 17:49:50 +0300

    maze: add all fuctional

--- src/maze/maze.js
+++ src/maze/maze.js
@@ -1,6 +1,13 @@
-let w = 5;
-let h = 7;
+let w = 7;
+let h = 4;
 let resolutionField = w * h;
+let wallCount = 1;
+// let button = document.querySelector('.button__maze');
+
+// button.addEventListener('click', () => {
+//     console.log(document.querySelector('.maze__measure-width').value)
+//     w = document.querySelector('.maze__measure-width').value;
+// })
 
 let maze = document.querySelector(".maze");
 
@@ -32,25 +39,50 @@ for (let i = 0; i < resolutionField; i++) {
     if (cell[0] == 1) {}
 }
 
-function generate() {
-    let posX = Math.round(Math.random() * (w - 1) + 1);
-    let posY = Math.round(Math.random() * (h - 1) + 1);
-    // console.log(posX, posY);
-    return [posX, posY];
+let wall;
+
+function createWall() {
+    for (let i = 0; i < wallCount; i++) {
+        function generate() {
+            let posX = Math.round(Math.random() * (w - 1) + 1);
+            let posY = Math.round(Math.random() * (h - 1) + 1);
+            // console.log(posX, posY);
+            return [posX, posY];
+        }
+
+        let coordinates = generate();
+
+        wall = document.querySelector(
+            '[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'
+        );
+        // not working
+        // while (wall.classList.contains("wall" || "exit" || "avatar")) {
+        //     let coordinates = generate();
+        //     wall = document.querySelector(
+        //         '[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'
+        //     );
+        // }
+        wall.classList.add("wall");
+        console.log(wall)
+            // console.log(wall);
+    }
 }
 
-let coordinates = generate();
-let wall = [
-    document.querySelector(
-        '[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'
-    ),
-];
+createWall();
 
 let avatar = [
     document.querySelector('[posX = "' + 1 + '"][posY = "' + 1 + '"]'),
 ];
 
-let exit = [document.querySelector('[posX = "' + w + '"][posY = "' + h + '"]')];
+// let exit = [document.querySelector('[posX = "' + w + '"][posY = "' + h + '"]')];
+
+let exit = document.querySelector(
+    '[posX = "' + w + '"][posY = "' + h + '"]'
+);
+
+exit.classList.add("exit");
+
+
 
 for (let i = 0; i < avatar.length; i++) {
     avatar[i].classList.add("avatar");
@@ -60,10 +92,114 @@ for (let i = 0; i < exit.length; i++) {
     exit[i].classList.add("exit");
 }
 
-for (let i = 0; i < wall.length; i++) {
-    wall[i].classList.add("wall");
+// for (let i = 0; i < wall.length; i++) {
+//     wall[i].classList.add("wall");
+// }
+
+let direction = 'right';
+
+function move() {
+    let coordinates = [
+        avatar[0].getAttribute("posX"),
+        avatar[0].getAttribute("posY"),
+    ];
+    avatar[0].classList.remove("avatar");
+    avatar.pop();
+    if (direction == 'right') {
+        if (coordinates[0] < w) {
+            // console.log('coordinates[0] :' + coordinates[0], "w:" + w);
+            avatar.unshift(
+                document.querySelector(
+                    '[posX = "' + (+coordinates[0] + 1) + '"][posY = "' + coordinates[1] + '"]'
+                )
+            );
+        } else {
+            coordinates[0] = w;
+            avatar.unshift(document.querySelector('[posX =  "' + (+coordinates[0]) + '"][posY = "' + coordinates[1] + '"]'));
+        }
+    } else if (direction == 'left') {
+        if (coordinates[0] > 1) {
+            // console.log(coordinates[0]);
+            avatar.unshift(
+                document.querySelector(
+                    '[posX = "' + (+coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'
+                )
+            );
+        } else {
+            avatar.unshift(document.querySelector('[posX =  "1"][posY = "' + coordinates[1] + '"]'));
+        }
+    } else if (direction == 'up') {
+        if (coordinates[1] < h) {
+            avatar.unshift(
+                document.querySelector(
+                    '[posX = "' + coordinates[0] + '"][posY = "' + (+coordinates[1] + 1) + '"]'
+                )
+            );
+        } else {
+            coordinates[1] = h;
+            console.log(coordinates[0]);
+            avatar.unshift(document.querySelector('[posX =  "' + coordinates[0] + '"][posY = "' + (+coordinates[1]) + '"]'));
+        }
+    } else if (direction == 'down') {
+        if (coordinates[1] > 1) {
+
+            avatar.unshift(
+                document.querySelector(
+                    '[posX = "' + coordinates[0] + '"][posY = "' + (+coordinates[1] - 1) + '"]'
+                )
+            );
+        } else {
+
+            avatar.unshift(document.querySelector('[posX =  "' + coordinates[0] + '"][posY = "1"]'));
+        }
+    }
+    if (avatar[0].getAttribute('posX') == wall.getAttribute('posX') && avatar[0].getAttribute('posY') == wall.getAttribute('posY')) {
+        document.querySelector('.field').classList.add('end__game');
+    }
+
+    if (avatar[0].getAttribute('posX') == exit.getAttribute('posX') && avatar[0].getAttribute('posY') == exit.getAttribute('posY')) {
+
+        document.querySelector('.field').classList.add('win__game');
+
+
+        avatar = [
+            document.querySelector('[posX = "' + 1 + '"][posY = "' + 1 + '"]'),
+        ];
+
+        exit = document.querySelector(
+            '[posX = "' + w + '"][posY = "' + h + '"]'
+        );
+        let time = setTimeout(() => { document.querySelector('.field').classList.remove('win__game') }, 1000);
+        time;
+
+        let audio = new Audio('https://zvukogram.com/mp3/cats/1354/pobednyiy-zvuk-fanfar.mp3');
+
+        audio.play();
+        wallCount++;
+        createWall();
+        console.log(wallCount);
+
+    }
+
+
+    // console.log('wallY:' + wall.getAttribute('posY'), 'exitX:' + wall.getAttribute('posX'));
+    avatar[0].classList.add("avatar");
 }
 
-// let avatar = [document.querySelector("[ posX = '1'][ posY = '1']")];
-// console.log(avatar);
-// avatar.classList.add("avatar");
+let interval = setInterval(move, 1300);
+
+window.addEventListener('keydown', function(e) {
+    if (e.keyCode == 37) {
+        direction = 'left';
+
+    } else if (e.keyCode == 38) {
+        direction = 'up';
+
+    } else if (e.keyCode == 39) {
+
+        direction = 'right';
+    } else if (e.keyCode == 40) {
+
+        direction = 'down';
+    }
+})
